156926ab5b92f35964c2e1f131c26a6a45c0df2c
Vibhavijai <vibhavijai03@gmail.com>
2024-06-13 10:48:07 -0500

    altered navigation,home and added new category,and few changes to new customer

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
 import {AccountsComponent} from "./accounts/accounts.component";
+import {HomeComponent} from "./home/home.component";
 import {NewCustomerComponent} from "./new-customer/new-customer.component";
 import {CustomerAccountsComponent} from "./customer-accounts/customer-accounts.component";
 
 const routes: Routes = [
   { path :"customers", component : CustomersComponent},
   { path :"accounts", component : AccountsComponent},
+  { path :"home", component :HomeComponent},
   { path :"new-customer", component : NewCustomerComponent},
   { path :"customer-accounts/:id", component : CustomerAccountsComponent},
 ];
